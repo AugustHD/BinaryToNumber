7ec00a969583daebb6c9499ca809c7659875e1ee
augu0577 <august.hawk@gmail.com>
2022-02-16 11:19:38 +0100

    Initial commit

---
+++ src/main/java/com/example/binarytonumber/BinaryToNumberApplication.java
@@ -0,0 +1,13 @@
+package com.example.binarytonumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryToNumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinaryToNumberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/binarytonumber/BinaryToNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.binarytonumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaryToNumberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
